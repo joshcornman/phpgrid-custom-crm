1e1bd22a6809c7f948b7f85e790d922fe04cbf36
Joshua Cornman <44935521+joshcornman@users.noreply.github.com>
2019-04-27 23:29:30 -0500

    Update footer.php
    Added Home Link

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
 <br /><br /><br /><br /><br />
 <div class="footer">Cornman Tech CRM - <strong>www.cornmantech.com</strong>. &copy; <?php echo date('Y'); ?>.</div>
-
+<a href="../index.php" target="_blank">Home</a>
 </body>
 </html>
